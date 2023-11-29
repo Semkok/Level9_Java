5f631f774c591dacf47d933d470af2451015acbb
Sem kok <semster0509@gmail.com>
2023-11-29 14:45:45 +0100

    done with random questionText onto timer

--- ThirtySeconds/src/QuestionPanel.java
+++ ThirtySeconds/src/QuestionPanel.java
@@ -1,9 +1,55 @@
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.Box;
+import javax.swing.JLabel;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import java.util.Random;
 
 public class QuestionPanel extends JPanel {
 	
+	JLabel questionLabel, fillerAnswer;
+	Questions questions = new Questions();
+	
+	Random random = new Random();
+	
+	int randomQuestionNum;
+	int randomFillerNum[] = new int[4];
+	
+	String currentAnswer = "";
 	
 	QuestionPanel(){
-		Questions questions = new Questions();
+		setBackground(new Color(133, 93, 20));
+		setVisible(true);
+		setCurrentText(questionLabel);
 	}
+	
+	private void setCurrentText(JLabel questionLabel) {
+		questionLabel = new JLabel();
+		questionLabel.setForeground(Color.white);
+		questionLabel.setFont(new Font("Serif", Font.PLAIN, 24));
+		questionLabel.setBounds(0, 10, 400, 200);	
+		randomizeQuestions();
+		System.out.println(currentAnswer);
+		questionLabel.setText("<html>"+ questions.questionsList.get(randomQuestionNum) + "<br>" 
+		        +questions.fillerAnswerList.get(randomFillerNum[0]) + "<br>" 
+				+questions.fillerAnswerList.get(randomFillerNum[1]) + "<br>"
+				+questions.fillerAnswerList.get(randomFillerNum[2]) + "<br>"
+				+questions.fillerAnswerList.get(randomFillerNum[3]) + "<br>" 
+				+ "</html>");
+		this.add(questionLabel);
+	}
+	
+	private void randomizeQuestions() {
+		 randomQuestionNum = random.nextInt(0,questions.questionsList.size());
+		 currentAnswer = questions.answerList.get(randomQuestionNum);
+		 for(int i = 0; i < randomFillerNum.length; i++ ) {
+			 randomFillerNum[i] = (randomQuestionNum *4) + i;
+		 }
+		 
+	}
+	
 }
--- ThirtySeconds/src/Questions.java
+++ ThirtySeconds/src/Questions.java
@@ -4,57 +4,36 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Questions {
-	ArrayList<String> questionsList = new ArrayList<String>();
-	ArrayList<String> answerList = new ArrayList<String>();
-	
+	final ArrayList<String> questionsList = new ArrayList<String>();
+	final ArrayList<String> answerList = new ArrayList<String>();
+	final ArrayList<String> fillerAnswerList = new ArrayList<String>();
 	
 	Questions(){
-		getQuestionsFile();
-//		getAnswersFile();
-		System.out.println(questionsList.get(0));
+		fillQuestionsAndAnswers();
 	}
 	
 	
-	private void getQuestionsFile() {
+	private void fillQuestionsAndAnswers() {
+		// this method gets all the questions and answers and fillerAnswers from the text file
 		try {
 			File myObj = new File("QuestionsAndAnswers.txt");
 		    Scanner myReader = new Scanner(myObj);
 		    while (myReader.hasNextLine()) {
 		      String data = myReader.nextLine();
-		      
-		      
-		      if(data.startsWith("question")) {
-		    	  
-		    	  String question = data.replace("question","");
-		    	  questionsList.add(question);
-		    	  System.out.println(question);
+		      if(data.startsWith("question:")) {
+		    	  String question = data.replace("question:","");
+		    	  // Trim to remove the unneeded whitespace
+		    	  questionsList.add(question.trim());
+		      }
+		      else if(data.startsWith("answer:")) {
+		    	  String answer = data.replace("answer:","");   	  
+		    	  answerList.add(answer.trim());
+		      }
+		      else if(data.startsWith("-")) {
+		    	  String fillerAnswer = data;
+		    	  fillerAnswerList.add(fillerAnswer.trim());
 		      }
 		    }
-		    
-		    myReader.close();
-		      
-		} catch(FileNotFoundException e){
-			System.out.println("An error occurred.");
-		      e.printStackTrace();
-		}
-	}
-	
-	private void getAnswersFile() {
-		try {
-			File myObj = new File("QuestionsAndAnswers.txt");
-		    Scanner myReader = new Scanner(myObj);
-		    while (myReader.hasNextLine()) {
-		      String data = myReader.nextLine();
-		      
-		      
-		      if(data.startsWith("answer:")) {
-		    	  
-		    	  String answer = data.replace("answer: ","");
-		    	  answerList.add(answer);
-		    	  System.out.println(answer);
-		      }
-		    }
-		    
 		    myReader.close();
 		      
 		} catch(FileNotFoundException e){
